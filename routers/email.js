925048a522419df9c0215b4a4667f0d4dcc132c9
k3naks <shibeko.egor@gmail.com>
2017-08-29 12:04:11 +0300

    Set restrictions for file size and file number

--- routers/email.js
+++ routers/email.js
@@ -8,7 +8,7 @@ const multer  = require('multer')
 const {controller, EmailValidator} = require('../server/api-services/email')
 
 const router = express.Router()
-const upload = multer()
+const upload = multer({ limits: {fileSize: 52428800, files: 10} })
 
 router.route('/send')
     .post(upload.array('attachments'), EmailValidator.getMailTemplate(), controller.sendEmail.bind(controller))
